8d36a408c22e6ca4f4f9f1c2c695c6726424d4bb
xander254 <alexkinyanjui356@gmail.com>
2024-07-30 14:15:53 +0300

    fixed some issues with the main file. Refactored removing the struct

--- main.h
+++ main.h
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 
 
-
 /*Function Protypes*/
 int _putchar(char b);
 void _print_number(int a);
@@ -16,26 +15,4 @@ int _print_string(char *str);
 int _print_percent(void);
 int function_specifier(const char *format,va_list args);
 int _printf(const char *format, ...);
-
-/* Function Prototypes */
-int _putchar(char, b);
-int _print_string(va_list, arguments);
-int _print_percent(va_list, arguments);
-int function_specifier(const char *format, va_list  arguments);
-int _printf_(const char *format, ...)
-
-/* struct specifier */
-/**
-  * struct specy - a struct func specifier and args
-  * @exact: character member holding the function specifier
-  * @f: pointer to the list of arguments used
-  */
-typedef struct specy
-{
-    char exact;
-    int (*f)(va_list);
-} specy_t;
-
-
 #endif /* MAIN_H */
-
