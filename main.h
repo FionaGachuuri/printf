7340234ff6bac8704b32dc1471de42da318d6b80
xander254 <alexkinyanjui356@gmail.com>
2024-07-30 14:04:50 +0300

    the project main file

--- main.h
+++ main.h
@@ -7,25 +7,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 /*Function Protypes*/
 int _putchar(char b);
 void _print_number(int a);
 int _print_string(char *str);
-void _print_percent(void);
-int function_specifier(const char *format, ...);
-
-
-/*struct specifier*/
-/**
-  * struct specy - a struct func pecifier and args
-  * @exact: character member holding the function specifier
-  * @f: pointer to the list of arguments used
-  */
-
-typedef struct specy
-{
-	char exact;
-	int (*f)(va_list);
-} specy_t;
-
+int _print_percent(void);
+int function_specifier(const char *format,va_list args);
+int _printf(const char *format, ...);
 #endif /* MAIN_H */
