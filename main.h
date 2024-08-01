8568665fbab6c34e48ca6270906423fc3443c4d0
xander254 <alexkinyanjui356@gmail.com>
2024-08-01 03:59:16 +0300

    main to support print octal

--- main.h
+++ main.h
@@ -18,4 +18,5 @@ int function_specifier(const char *format, va_list args);
 int _printf(const char *format, ...);
 int _print_binary(unsigned int number);
 int _print_hexadecimal(unsigned int number, int uppercase);
+int _print_octal(unsigned int number);
 #endif /* MAIN_H */
